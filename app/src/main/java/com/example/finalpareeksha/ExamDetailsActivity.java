c6842ea6f5ddc794ae96fc8548a643a6edb9d0c1
MandaarRao <82019376+MandaarRao612@users.noreply.github.com>
2021-05-24 14:10:20 +0530

    Change 1

---
+++ app/src/main/java/com/example/finalpareeksha/ExamDetailsActivity.java
@@ -0,0 +1,14 @@
+package com.example.finalpareeksha;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class ExamDetailsActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_exam_details);
+    }
+}
